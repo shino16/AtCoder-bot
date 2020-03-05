9bfd6410c965549b338676d9a17a634a4859b251
shino-sky <ms_020916@yahoo.co.jp>
2020-03-06 06:09:49 +0900

    assign roles on getting ready

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 import discord
+import os
+import psycopg2
 
-token = 'Njg1MTU0MjM0OTE5OTQ0MjE5.XmEjVA.pbsz8FPjk3kZ5siqgz43noL2fLM'
+
+TOKEN = 'Njg1MTU0MjM0OTE5OTQ0MjE5.XmEjVA.pbsz8FPjk3kZ5siqgz43noL2fLM'
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print('Ready!')
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            for guild in client.guilds:
+                for member in guild.members:
+                    if not member.bot:
+                        update_color(member, cur)
+                        await set_role(member, cur)
+    await client.logout()
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
+def update_color(member, cur):
+    color = "blue"
+    cur.execute("SELECT COUNT(*) FROM profile WHERE discord_name = "
+                f"'{member.name}#{member.discriminator}'")
+    count = cur.fetchone()
+    if count:
+        cur.execute(f"UPDATE profile SET color = '{color}' WHERE discord_name = "
+                    f"'{member.name}#{member.discriminator}'")
+    else:
+        cur.execute("INSERT INTO profile (discord_name, atcoder_name, color)"
+                    "VALUES ('{member.name}#{member.discriminator}'"
+                    "        '{member.name}', '{color}')")
+
+async def set_role(member, cur):
+    print(f"Setting the role of {member.name}#{member.discriminator}")
+    cur.execute("SELECT color FROM profile WHERE discord_name = "
+                f"'{member.name}#{member.discriminator}'")
+    record = cur.fetchone()
+    if not record:
         return
-    print("Received: " + message.content)
+    color = record[0]
+    roles = [role for role in member.guild.roles
+             if role.name.startswith(color)]
+    if roles:
+        await member.edit(roles=roles)
+        print("Set: " + ", ".join(role.name for role in roles))
+    else:
+        print("[Warning] Role not found")
 
-client.run(token)
+
+client.run(TOKEN)
